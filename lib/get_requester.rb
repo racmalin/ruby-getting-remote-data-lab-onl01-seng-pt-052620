310c1316124898e4832f26534d902a3d8dccc129
Kevin C Malin <racmalin@yahoo.com>
2020-07-03 01:04:26 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -6,18 +6,18 @@
 
 
 class GetRequester
-  
+ 
   def initialize(url)
     @url = url
- 
+  end
+
   def get_response_body
-    uri = URI.parse("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
-    response = Net :: HTTP.get_response(uri)
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
     response.body
+  end
 
   def parse_json
     JSON.parse(get_response_body)
-   end
-   end
   end
 end
