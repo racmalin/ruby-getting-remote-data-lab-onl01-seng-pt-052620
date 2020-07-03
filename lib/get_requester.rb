b3b35d15a1d6cef189af79960deae189e7ee95a6
Kevin C Malin <racmalin@yahoo.com>
2020-07-03 00:46:43 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,23 @@
-# Write your code here
+# require 'open-uri'
+# require 'net/http'
+# require 'json'
+
+
+
+
+class GetRequester
+  
+  def initialize(url)
+    @url = url
+ 
+  def get_response_body
+    uri = URI.parse("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+    response = Net :: HTTP.get_response(uri)
+    response.body
+
+  def parse_json
+    JSON.parse(get_response_body)
+   end
+   end
+  end
+end
